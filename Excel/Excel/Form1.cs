fed9ad947f41f317af63b796bf4fc17c07e83bca
SEONWOOHONGMIN <seonwoohongmin@gmail.com>
2023-07-11 17:43:41 +0900

    Excel get the first worksheet and Data Input and Save Excel but The xlsx extension is not allowed

--- Excel/Excel/Form1.cs
+++ Excel/Excel/Form1.cs
@@ -1,12 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using Microsoft.Office.Interop.Excel;
+using System.Runtime.InteropServices;
 
 namespace Excel
 {
@@ -19,7 +16,67 @@ namespace Excel
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            Console.Write("githubtest");
+            List<string> testData = new List<string>()
+            { "Excel", "Access", "Word", "OnceNote"};
+
+            Microsoft.Office.Interop.Excel.Application excelApp = null;
+            Workbook wb = null;
+            Worksheet ws = null;
+
+            try
+            {
+                // Excel get the first worksheet
+                excelApp = new Microsoft.Office.Interop.Excel.Application();
+                wb = excelApp.Workbooks.Add();
+                ws = wb.Worksheets.get_Item(1) as Microsoft.Office.Interop.Excel.Worksheet;
+
+                // Data Input
+                int r = 1;
+                foreach (var d in testData)
+                {
+                    ws.Cells[r, 1] = d;
+                    r++;
+                }
+
+                // Save Excel
+                wb.SaveAs(@"C:\Users\Hongmin\Desktop/test.xls", Microsoft.Office.Interop.Excel.XlFileFormat.xlWorkbookNormal); // ** xlsx 확장자 안되는 것 확인 ㅠ
+                wb.Close(true);
+                excelApp.Quit();
+
+            }
+            catch (Exception ex)
+            {
+
+            }
+            finally
+            {
+                // Clean up
+                ReleaseExcelObject(ws);
+                ReleaseExcelObject(wb);
+                ReleaseExcelObject(excelApp);
+            }
         }
+
+        private static void ReleaseExcelObject(object obj)
+        {
+            try
+            {
+                if (obj != null)
+                {
+                    Marshal.ReleaseComObject(obj);
+                    obj = null;
+                }
+            }
+            catch (Exception ex)
+            {
+                obj = null;
+                throw ex;
+            }
+            finally
+            {
+                GC.Collect();
+            }
+        }
+
     }
 }
