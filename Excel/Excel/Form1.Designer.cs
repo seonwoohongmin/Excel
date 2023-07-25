1f76075468bc66f47b5d03a89e4e150adc6e0bc9
SEONWOOHONGMIN <seonwoohongmin@gmail.com>
2023-07-26 01:40:58 +0900

    to read all the contents of the first sheet and Excel Process kill

--- Excel/Excel/Form1.Designer.cs
+++ Excel/Excel/Form1.Designer.cs
@@ -28,21 +28,34 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(672, 316);
+            this.textBox1.TabIndex = 0;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(716, 375);
+            this.Controls.Add(this.textBox1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
--- Excel/Excel/Form1.cs
+++ Excel/Excel/Form1.cs
@@ -4,11 +4,18 @@ using System.ComponentModel;
 using System.Windows.Forms;
 using Microsoft.Office.Interop.Excel;
 using System.Runtime.InteropServices;
+using System.IO;
+
 
 namespace Excel
 {
     public partial class Form1 : Form
     {
+        #region Excel Process ID
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        static extern uint GetWindowThreadProcessId(IntPtr hWnd, out uint lpdwProcessId);
+        #endregion
+
         public Form1()
         {
             InitializeComponent();
@@ -19,9 +26,14 @@ namespace Excel
             List<string> testData = new List<string>()
             { "Excel", "Access", "Word", "OnceNote"};
 
+            // Save Excel Variable
             Microsoft.Office.Interop.Excel.Application excelApp = null;
             Workbook wb = null;
             Worksheet ws = null;
+
+            String filePath = null;
+            String data = null;
+            uint processId = 0;
 
             try
             {
@@ -29,6 +41,8 @@ namespace Excel
                 excelApp = new Microsoft.Office.Interop.Excel.Application();
                 wb = excelApp.Workbooks.Add();
                 ws = wb.Worksheets.get_Item(1) as Microsoft.Office.Interop.Excel.Worksheet;
+
+                filePath = @"C:\Users\Hongmin\Desktop/test.xls";
 
                 // Data Input
                 int r = 1;
@@ -38,8 +52,28 @@ namespace Excel
                     r++;
                 }
 
-                // Save Excel
-                wb.SaveAs(@"C:\Users\Hongmin\Desktop/test.xls", Microsoft.Office.Interop.Excel.XlFileFormat.xlWorkbookNormal); // ** xlsx 확장자 안되는 것 확인 ㅠ
+                if (File.Exists(filePath))
+                    File.Delete(filePath);
+
+                wb.SaveAs(filePath, Microsoft.Office.Interop.Excel.XlFileFormat.xlWorkbookNormal); // ** xlsx 확장자 안되는 것 확인 ㅠ
+
+                wb = excelApp.Workbooks.Open(Filename: @filePath);
+                excelApp.Visible = false;
+                Range range = ws.UsedRange;
+
+
+                for (int i = 1; i <= range.Rows.Count; ++i)
+                {
+                    for (int j = 1; j <= range.Columns.Count; ++j)
+                    {
+                        data += ((range.Cells[i, j] as Range).Value2.ToString() + " ");
+                    }
+                    data += "\n";
+                }
+
+                textBox1.Text = data;
+
+                // close Excel
                 wb.Close(true);
                 excelApp.Quit();
 
@@ -50,10 +84,22 @@ namespace Excel
             }
             finally
             {
+
+                GetWindowThreadProcessId(new IntPtr(excelApp.Hwnd), out processId); // Get the Excel PID
+
+                if (processId != 0) //Excel Process kill
+                {
+                    System.Diagnostics.Process excelProcess = System.Diagnostics.Process.GetProcessById((int)processId);
+                    excelProcess.CloseMainWindow();
+                    excelProcess.Refresh();
+                    excelProcess.Kill();
+                }
+
                 // Clean up
                 ReleaseExcelObject(ws);
                 ReleaseExcelObject(wb);
                 ReleaseExcelObject(excelApp);
+                
             }
         }
 
